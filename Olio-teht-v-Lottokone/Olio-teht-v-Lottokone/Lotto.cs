6ca3bb4f4457287f5063ec6b98d2081428792484
h8324 <h8324@OHJE-322-WS-09>
2017-03-08 20:59:59 +0200

    Initial commit

---
+++ Olio-teht-v-Lottokone/Olio-teht-v-Lottokone/Lotto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_teht_v_Lottokone
+{
+    class Lotto
+    {
+        static Random random = new Random();
+
+        public static List<int> GenerateRandom(int count)
+        {
+            // generate count random values.
+            HashSet<int> candidates = new HashSet<int>();
+            while (candidates.Count < count)
+            {
+                // May strike a duplicate.
+                candidates.Add(random.Next());
+            }
+
+            // load them in to a list.
+            List<int> result = new List<int>();
+            result.AddRange(candidates);
+
+            // shuffle the results:
+            int i = result.Count;
+            while (i > 1)
+            {
+                i--;
+                int k = random.Next(i + 1);
+                int value = result[k];
+                result[k] = result[i];
+                result[i] = value;
+            }
+            return result;
+        }
+        public static void Main()
+        {
+            List<int> vals = GenerateRandom(6);
+        }
+    }
+
+}
